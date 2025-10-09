6fe41f8b91e925915872ccd62b5ba76d57a3fbd2
anderson <nobreanderson@hotmail.com>
2025-10-08 21:44:45 -0300

    Adiciona paginação DRF e mantém compatibilidade com exercícios anteriores

--- api_new/urls.py
+++ api_new/urls.py
@@ -8,14 +8,12 @@ from api_new.viewsets.product_viewsets import ProductViewSet
 from api_new.viewsets.category_viewsets import CategoryViewSet
 
 router = DefaultRouter()
-<<<<<<< HEAD
 router.register(r'items', ItemViewSet, basename='item')
-=======
-router.register(r'items', ItemViewSet)
 router.register(r'orders', OrderViewSet)
 router.register(r'products', ProductViewSet)
 router.register(r'categories', CategoryViewSet)
->>>>>>> 45dda2e (Adicionados ViewSets e Serializers para Order, Product e Category sem alterar Item existente)
+
+
 
 urlpatterns = [
     path('', include(router.urls)),
